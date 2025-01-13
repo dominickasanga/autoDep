c1f8e29579a17ab6964f8c1d4222b0aa1dba0291
Dominic kasanga <dominickasanga@gmail.com>
2025-01-13 13:39:23 +0200

    modified:   app/utils/host.py

--- app/utils/host.py
+++ app/utils/host.py
@@ -34,7 +34,7 @@ async def update_remote_host(user_name: str, ip_address: str) -> str:
 
     try:
         print_stage("INITIALIZING DEPLOYMENT")
-        print_action(f"Connecting to remote host: {user_name}{'@'}{ip_address}")
+        print_action(f"Connecting to remote host: {ip_address}")
         
         client = await conect_to_remote_host(ip_address, user_name)
         app_dirs = os.getenv('APP_DIRS').split(',')
@@ -46,6 +46,9 @@ async def update_remote_host(user_name: str, ip_address: str) -> str:
         if isinstance(client, AsyncParamikoSSHClient):
             try:
                 await client.clsConnect()
+                print("##########################################")
+                print(f"Connect status: {client.is_connected()}")
+                print("##########################################")
                 if client.is_connected() == False:
                     client.close()
                     return 
